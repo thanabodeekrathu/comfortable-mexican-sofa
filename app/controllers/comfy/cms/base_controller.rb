43f80939857e619cdb7a50f4886b4e5475846d06
thanabodee <thanabodee@bananacoding.com>
2020-11-13 16:06:57 +0700

    show logs in base_controller and cms.rb

--- app/controllers/comfy/cms/base_controller.rb
+++ app/controllers/comfy/cms/base_controller.rb
@@ -9,15 +9,32 @@ class Comfy::Cms::BaseController < ComfortableMexicanSofa.config.public_base_con
 protected
 
   def load_cms_site
+    puts "=====+++++----- cms/base_controller.rb"
     @cms_site ||=
       if params[:site_id]
+        puts "=====+++++----- site id"
+
+        ap params[:site_id]
         ::Comfy::Cms::Site.find_by_id(params[:site_id])
       else
+        puts "=====+++++----- host_with_port"
+        ap request.host_with_port.downcase
+
+        puts "=====+++++----- fullpath"
+        ap request.fullpath
         ::Comfy::Cms::Site.find_site(request.host_with_port.downcase, request.fullpath)
       end
+    puts "=====+++++----- cms_site"
+    ap @cms_site
 
     if @cms_site
       if @cms_site.path.present? && !params[:site_id]
+        puts "=====+++++----- params[:cms_path]"
+        ap params[:cms_path]
+
+        puts "=====+++++----- @cms_site.path"
+        ap @cms_site.path
+
         if params[:cms_path]&.match(%r{\A#{@cms_site.path}})
           params[:cms_path].gsub!(%r{\A#{@cms_site.path}}, "")
           params[:cms_path]&.gsub!(%r{\A/}, "")
@@ -28,6 +45,8 @@ protected
     else
       raise ActionController::RoutingError, "Site Not Found"
     end
+    puts "=====+++++----- end cms/base_controller.rb"
+
   end
 
 end
--- lib/comfortable_mexican_sofa/routes/cms.rb
+++ lib/comfortable_mexican_sofa/routes/cms.rb
@@ -3,16 +3,25 @@
 class ActionDispatch::Routing::Mapper
 
   def comfy_route_cms(options = {})
+    puts "=====+++++----- route/cms.rb"
     ComfortableMexicanSofa.configuration.public_cms_path = options[:path]
+    puts "=====+++++----- options[:path]"
+    ap options[:path]
+    puts "=====+++++----- ComfortableMexicanSofa.configuration.public_cms_path"
+    ap ComfortableMexicanSofa.configuration.public_cms_path
 
     scope module: :comfy, as: :comfy do
+      puts "=====+++++----- comfy"
+      ap comfy
       namespace :cms, path: options[:path] do
+
         get "cms-css/:site_id/:identifier(/:cache_buster)" => "assets#render_css", as: "render_css"
         get "cms-js/:site_id/:identifier(/:cache_buster)"  => "assets#render_js",  as: "render_js"
 
         get "(*cms_path)" => "content#show", as: "render_page", action: "/:format"
       end
     end
+    puts "=====+++++----- end route/cms.rb"
   end
 
 end
